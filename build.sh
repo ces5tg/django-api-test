509618945517c360f4dd3d767d519d3dfad43e17
ces5tg <cesar.pacho@tecsup.edu.pe>
2023-04-14 22:45:20 -0500

    riquirimint

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install -r requirements.txt
+pip install -r requiriments.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
